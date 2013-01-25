6664c71e3a69f26855237f5911bcf506ebb93de2
anindian <disturbme@live.in>
2013-01-25 12:48:56 +0530

    Started project 001

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,11 @@
+class AccountController < ApplicationController
+
+  def login
+
+  end
+
+  def logout
+
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cschools::Application.config.secret_token = 'add1f7aa802019fd6b256853a468785fe87c59e9a3d7b97e81222b9c4c4e92fa4b3715aae144d8ba7394fc15b8327520a12ad05d1bb2537ec05505b3676a64c7'
